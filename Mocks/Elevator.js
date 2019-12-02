6020fbf69060a24a2371d23aadf480514f7835aa
James Lendrem <James@kinocreative.co.uk>
2019-12-02 09:35:43 +0000

    Mock up elevator and floor object.

---
+++ Mocks/Elevator.js
@@ -0,0 +1,24 @@
+
+class Elevator {
+
+    //Events
+    // idle
+    // floor_button_pressed
+    // passing_floor
+    // stopped_at_floor
+    on(event, listener) {};
+
+    goToFloor(floorNum)  {};
+    stop() {};
+    currentFloor() {};
+    goingUpIndicator() {};
+    goingDownIndicator() {};
+    maxPassengerCount() {};
+    loadFactor() {};
+    destinationDirection() {};
+    destinationQueue() {};
+    checkDestinationQueue() {}
+    getPassedFloors() {};
+
+
+}
---
+++ Mocks/Floor.js
@@ -0,0 +1,9 @@
+
+class Floor {
+
+    // up_button_pressed
+    // down_button_pressed
+    on(event, listener) {};
+
+    floorNum() {};
+}
